bc4df75da850ac28aa91657e177874ec8b02955b
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-25 13:11:25 +0800

    fix: typo module imports

--- src/components/PatientDetailPage/index.tsx
+++ src/components/PatientDetailPage/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import FemaleIcon from '@mui/icons-material/Female';
-import MaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
 
 import {
 	Diagnosis,
